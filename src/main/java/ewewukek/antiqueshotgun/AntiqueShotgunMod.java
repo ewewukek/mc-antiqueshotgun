fb40929352a9fde9f972ccddb30d2dd65767aa9c
ewewukek <ewewukek@gmail.com>
2021-04-13 20:37:29 +0300

    mod template

---
+++ src/main/java/ewewukek/antiqueshotgun/AntiqueShotgunMod.java
@@ -0,0 +1,8 @@
+package ewewukek.antiqueshotgun;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AntiqueShotgunMod.MODID)
+public class AntiqueShotgunMod {
+    public static final String MODID = "antiqueshotgun";
+}
