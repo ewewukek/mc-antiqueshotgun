d52a63678df3afa4483441c5d367e91524e2010d
ewewukek <ewewukek@gmail.com>
2021-04-24 01:39:09 +0300

    give elder hunter a shotgun

--- src/main/java/ewewukek/antiqueshotgun/entity/ElderHunterEntity.java
+++ src/main/java/ewewukek/antiqueshotgun/entity/ElderHunterEntity.java
@@ -1,16 +1,20 @@
 package ewewukek.antiqueshotgun.entity;
 
+import ewewukek.antiqueshotgun.AntiqueShotgunMod;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.monster.AbstractIllagerEntity;
 import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 
 public class ElderHunterEntity extends AbstractIllagerEntity {
     public ElderHunterEntity(EntityType<? extends ElderHunterEntity> type, World worldIn) {
         super(type, worldIn);
+        setItemStackToSlot(EquipmentSlotType.MAINHAND, new ItemStack(AntiqueShotgunMod.ANTIQUE_SHOTGUN));
     }
 
     public static AttributeModifierMap createEntityAttributes() {
