cee4e94c4ab6f5b9f857ab13dc7b829c6bd770b5
ewewukek <ewewukek@gmail.com>
2021-04-24 01:37:53 +0300

    add arms to the model

--- src/main/java/ewewukek/antiqueshotgun/client/ElderHunterModel.java
+++ src/main/java/ewewukek/antiqueshotgun/client/ElderHunterModel.java
@@ -14,8 +14,9 @@ public class ElderHunterModel<T extends ElderHunterEntity> extends EntityModel<T
     private final ModelRenderer head;
     private final ModelRenderer nose;
     private final ModelRenderer body;
-    private final ModelRenderer arms;
-    private final ModelRenderer mirrored;
+    private final ModelRenderer arms_folded;
+    private final ModelRenderer right_arm;
+    private final ModelRenderer left_arm;
     private final ModelRenderer right_leg;
     private final ModelRenderer left_leg;
 
@@ -39,16 +40,24 @@ public class ElderHunterModel<T extends ElderHunterEntity> extends EntityModel<T
         body.setTextureOffset(32, 25).addBox(-4.0F, 0.0F, -3.0F, 8.0F, 12.0F, 6.0F, 0.0F, false);
         body.setTextureOffset(0, 0).addBox(-4.0F, 0.0F, -3.0F, 8.0F, 18.0F, 6.0F, 0.5F, false);
 
-        arms = new ModelRenderer(this);
-        arms.setRotationPoint(0.0F, 2.0F, 0.0F);
-        setRotationAngle(arms, -0.9163F, 0.0F, 0.0F);
-        arms.setTextureOffset(48, 14).addBox(-4.0F, 2.0F, -2.0F, 8.0F, 4.0F, 4.0F, 0.0F, false);
-        arms.setTextureOffset(48, 59).addBox(-8.0F, -2.0F, -2.0F, 4.0F, 8.0F, 4.0F, 0.0F, false);
+        arms_folded = new ModelRenderer(this);
+        arms_folded.setRotationPoint(0.0F, 2.0F, 0.0F);
+        setRotationAngle(arms_folded, -0.9163F, 0.0F, 0.0F);
+        arms_folded.setTextureOffset(48, 14).addBox(-4.0F, 2.0F, -2.0F, 8.0F, 4.0F, 4.0F, 0.0F, false);
+        arms_folded.setTextureOffset(48, 59).addBox(-8.0F, -2.0F, -2.0F, 4.0F, 8.0F, 4.0F, 0.0F, false);
 
-        mirrored = new ModelRenderer(this);
+        ModelRenderer mirrored = new ModelRenderer(this);
         mirrored.setRotationPoint(0.0F, 22.0F, 0.0F);
-        arms.addChild(mirrored);
+        arms_folded.addChild(mirrored);
         mirrored.setTextureOffset(32, 59).addBox(4.0F, -24.0F, -2.0F, 4.0F, 8.0F, 4.0F, 0.0F, false);
+
+        right_arm = new ModelRenderer(this);
+        right_arm.setRotationPoint(5.0F, 2.0F, 0.0F);
+        right_arm.setTextureOffset(48, 43).addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
+
+        left_arm = new ModelRenderer(this);
+        left_arm.setRotationPoint(-5.0F, 2.0F, 0.0F);
+        left_arm.setTextureOffset(32, 43).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
 
         right_leg = new ModelRenderer(this);
         right_leg.setRotationPoint(2.0F, 12.0F, 0.0F);
@@ -70,7 +79,9 @@ public class ElderHunterModel<T extends ElderHunterEntity> extends EntityModel<T
         head.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn);
         nose.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn);
         body.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn);
-        arms.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn);
+//        arms_folded.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn);
+        right_arm.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn);
+        left_arm.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn);
         right_leg.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn);
         left_leg.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn);
     }
